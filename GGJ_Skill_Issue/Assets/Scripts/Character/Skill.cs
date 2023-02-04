dfbad16f67097b11c0cca886aca35ef585f30fce
GabrielKwa <0127936@kdu-online.com>
2023-02-04 18:51:51 +0800

    Skills

--- GGJ_Skill_Issue/Assets/Scripts/Character/Skill.cs
+++ GGJ_Skill_Issue/Assets/Scripts/Character/Skill.cs
@@ -5,11 +5,44 @@ using UnityEngine;
 public class Skill : MonoBehaviour
 {
     public Player player;
-    public int attackDamage, attackRange, mpCost;
-    public float cooldownInSecs;
+    int attackDamage, attackRange, mpCost;
+    float cooldownInSecs, cooldownCache;
+    bool isOnCooldown, drainCooldown;
 
     public virtual void OnSkillCast() { }
+    void SetOnCooldown(bool state)
+    {
+        drainCooldown = state;
+        if (drainCooldown && !isOnCooldown)
+        {
+            cooldownCache = cooldownInSecs;
+        }
+        if (!state)
+        {
+            isOnCooldown = false;
+        }
+    }
 
+    void DrainCooldown()
+    {
+        if (drainCooldown)
+        {
+            isOnCooldown = drainCooldown;
+            if (cooldownCache <= 0)
+            {
+                drainCooldown = false;
+                isOnCooldown = false;
+            }
+            else
+            {
+                cooldownCache -= Time.deltaTime;
+            }
+        }
+    }
 
+    private void Update()
+    {
+        DrainCooldown();
+    }
 
 }
